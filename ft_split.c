e8b1c55d4968ffe76b69ad3f0ba23733f75a86c5
Josefig <josefiggaming@gmail.com>
2021-03-13 17:12:53 +0000

    steal dimitri's ft_split

--- ft_split.c
+++ ft_split.c
@@ -12,108 +12,47 @@
 
 #include "libft.h"
 
-static int		ft_hm(char const *s, char c)
+static size_t	get_length(const char *s, char c)
 {
-	size_t	nbr;
-	int		i;
-
-	nbr = 0;
-	i = 0;
-	while (s[i])
-	{
-		while (s[i] == c)
-			i++;
-		if (i > 0 && s[i] && s[i - 1] == c)
-			nbr++;
-		if (s[i])
-			i++;
-	}
-	if (nbr == 0 && s[i - 1] == c)
-		return (0);
-	if (s[0] != c)
-		nbr++;
-	return (nbr);
-}
-
-static char		**ft_mal(char **strs, char const *s, char c)
-{
-	size_t	count;
-	int		i;
-	int		h;
+	int count;
 
 	count = 0;
-	i = 0;
-	h = 0;
-	while (s[h])
+	while (*s)
 	{
-		if (s[h] != c)
-			count++;
-		else if (h > 0 && s[h - 1] != c)
-		{
-			strs[i] = malloc(sizeof(char) * (count + 1));
-			if (!strs[i])
-				return (0);
-			count = 0;
-			i++;
-		}
-		if (s[h + 1] == '\0' && s[h] != c)
-			if (!(strs[i] = malloc(sizeof(char) * count + 1)))
-				return (0);
-		h++;
+		if (*s++ == c)
+			continue ;
+		count++;
+		while (*s && *s != c)
+			s++;
 	}
-	return (strs);
-}
-
-static char		**ft_cpy(char **strs, char const *s, char c)
-{
-	int i;
-	int j;
-	int h;
-
-	i = 0;
-	j = 0;
-	h = 0;
-	while (s[h])
-	{
-		if (s[h] != c)
-			strs[i][j++] = s[h];
-		else if (h > 0 && s[h - 1] != c)
-			if (h != 0)
-			{
-				strs[i][j] = '\0';
-				j = 0;
-				i++;
-			}
-		if (s[h + 1] == '\0' && s[h] != c)
-			strs[i][j] = '\0';
-		h++;
-	}
-	return (strs);
+	return (count);
 }
 
 char			**ft_split(char const *s, char c)
 {
-	char	**rtn;
-	int		nbr_w;
+	char	**strs;
+	char	**sav_strs;
+	char	*sav_s;
 
-	if (!s || !*s)
-	{
-		if (!(rtn = malloc(sizeof(char *) * 1)))
-			return (NULL);
-		*rtn = (void *)0;
-		return (rtn);
-	}
-	nbr_w = ft_hm(s, c);
-	rtn = malloc(sizeof(char *) * (nbr_w + 1));
-	if (!rtn)
+	if (!s)
 		return (0);
-	if (ft_mal(rtn, s, c) != 0)
-		ft_cpy(rtn, s, c);
-	else
+	if (!(strs = malloc((get_length(s, c) + 1) * sizeof(char *))))
+		return (0);
+	sav_strs = strs;
+	while (*s)
 	{
-		free(rtn);
-		return (NULL);
+		if (*s == c)
+		{
+			s++;
+			continue ;
+		}
+		sav_s = (char *)s;
+		while (*s && *s != c)
+			s++;
+		if (!(*strs = malloc((s - sav_s + 1) * sizeof(char))))
+			return (0);
+		ft_strlcpy(*strs++, sav_s, s - sav_s + 1);
 	}
-	rtn[nbr_w] = (void *)0;
-	return (rtn);
+	*strs = 0;
+	return (sav_strs);
 }
