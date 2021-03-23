55433fb014151e377ed2d4ca6197e48ac551f606
Josefig <josefiggaming@gmail.com>
2021-03-23 11:14:05 +0000

    add get_next_line to Libft

---
+++ get_next_line.h
@@ -0,0 +1,16 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+int		get_next_line(int fd, char **line);
+int		has_nl(char *str);
+char	*join_str(const char *str1, const char *str2);
+size_t	ft_strlen(const char *s);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+#endif
--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include "get_next_line.h"
 # include <stdlib.h>
 # include <unistd.h>
 
