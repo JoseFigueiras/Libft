3311986ab6a589209166e8601f2508d291946a7a
Josefig <josefiggaming@gmail.com>
2021-04-01 13:37:05 +0100

    remove libft functions from gnl_utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -31,35 +31,3 @@ char	*join_str(char const *str1, char const *str2)
 	free((char *)str1);
 	return (ret);
 }
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	char	*d;
-	char	*s;
-
-	d = (char *)dst;
-	s = (char *)src;
-	if (dst == src)
-		return (dst);
-	if (s < d)
-	{
-		while (len--)
-			*(d + len) = *(s + len);
-		return (dst);
-	}
-	while (len--)
-		*d++ = *s++;
-	return (dst);
-}
