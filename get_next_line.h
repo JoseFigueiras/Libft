adc384c5529769df472a516d80d262caf3cde943
Josefig <josefiggaming@gmail.com>
2021-04-02 12:49:55 +0100

    remove gnl duplicate prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -10,7 +10,5 @@
 int		get_next_line(int fd, char **line);
 int		has_nl(char *str);
 char	*join_str(const char *str1, const char *str2);
-size_t	ft_strlen(const char *s);
-void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
